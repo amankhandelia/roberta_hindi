42a8f0b2ae052585c9f4dced3b6e7261f656d0ed
Rahul Dev <korrarahuldev@gmail.com>
2021-07-09 12:35:43 +0530

    Update prepare_datasets.sh

--- datasets/prepare_datasets.sh
+++ datasets/prepare_datasets.sh
@@ -1,3 +1,11 @@
 kaggle datasets download disisbig/hindi-text-short-summarization-corpus
 unzip hindi-text-short-summarization-corpus.zip -d hindi-text-short-summarization-corpus/
 
+kaggle datasets download mathurinache/samanantar --file "final_data/en-hi/train.hi"
+unzip train.hi.zip -d samanantar/
+
+kaggle datasets download crazydiv/oldnewspapershindi
+unzip oldnewspapershindi.zip -d oldnewspapershindi/
+
+kaggle datasets download disisbig/hindi-text-short-and-large-summarization-corpus
+unzip hindi-text-short-and-large-summarization-corpus.zip -d hindi-text-short-and-large-summarization-corpus/
